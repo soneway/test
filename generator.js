0b69dafdee919598e9baec8f8539be9cdc4faa5a
宋均均 <songjunjun@bytedance.com>
2021-06-15 15:38:06 +0800

    feat: test

--- generator.js
+++ generator.js
@@ -4,40 +4,58 @@ function* generator() {
       resolve(100);
     }, 1000);
   });
-  console.log(r1);
+  console.log('r1', r1); // 1000ms之后打印100
 
   const r2 = yield new Promise((resolve) => {
     setTimeout(() => {
       resolve(200);
     }, 2000);
   });
-  console.log(r2);
+  console.log('r2', r2); // 2000ms之后打印200
 
-  const r3 = yield new Promise((resolve, reject) => {
+  const r3 = yield new Promise((resolve) => {
     setTimeout(() => {
-      reject(300);
+      resolve(300);
     }, 3000);
   });
-  console.log(r3);
+  console.log('r3', r3); // 3000ms之后打印300
+
+  return 400;
 }
 
-function run(gen) {
-  const g = gen();
+// 完成run函数
+function run(generator) {
+  return new Promise((resolve, reject) => {
+    const g = generator();
 
-  function step(value) {
-    const res = g.next(value);
+    function next(value) {
+      // 获取下个yield, 并把上个yield表达式的值传递
+      const res = g.next(value);
+      // console.log('res', res);
 
-    if (res.done) {
-      return res.value;
+      // 生成器结束
+      if (res.done) {
+        return resolve(res.value);
+      }
+
+      // 生成器未结束
+      // resolve yield表达式
+      Promise.resolve(res.value)
+        .then((value) => {
+          // 继续运行生成器, 并把当前resolve的值传过去
+          next(value);
+        })
+        .catch(reject);
     }
 
-    Promise.resolve(res.value).then((value) => {
-      // console.log(111111, value);
-      step();
-    });
-  }
-
-  step();
+    next();
+  });
 }
 
-run(generator);
+run(generator)
+  .then((res) => {
+    console.log('then', res); // 400
+  })
+  .catch((e) => {
+    console.log('catch', e); // 运行过程中异常可捕获
+  });
